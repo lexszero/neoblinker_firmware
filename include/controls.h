7ca2ef520bf6bd5688fc56bd0f2dd65c3c2b77a3
Alexey Ignatov <lexszero@gmail.com>
2018-11-22 07:32:07 +0200

    Lots of business logic improvements

--- include/controls.h
+++ include/controls.h
@@ -2,37 +2,45 @@
 #include "core.h"
 #include <NeoAnimationFX.h>
 
-#define CTL_SPEED_MAX 2000
-
+#define SEGMENTS_COUNT 2
 #define SEGMENTS \
-	SEGMENT_ITEM(0, all, 0, (LED_COUNT-1), false)
+	SEGMENT_ITEM(0, A,				0,	(LED_COUNT/2-1),	false) \
+	SEGMENT_ITEM(1, B,	(LED_COUNT/2),	(LED_COUNT-1),		true)
 
 #define SEG(name) __cat(seg_, name)
 
 class Controls : ControlsBase {
 public:
 	Controls();
-	void begin();
 
 	std::unordered_map<std::string, AbstractControl*> _all;
+
+#ifdef BOARD_RELAY
+	ControlSwitch relay;
+#endif
 
 	ControlSwitch artnet;
 	ControlRange<uint8_t, 255> brightness;
 	ControlPushButton trigger;
 
+	ControlSwitch sync;
+
 	struct Segment {
 		Segment(const std::string&& prefix, uint8_t _id);
+		Segment& operator=(const Segment& seg);
 
 		uint8_t id;
 
 		ControlRange<uint8_t, MODE_COUNT> mode;
-		ControlRange<uint16_t, CTL_SPEED_MAX> speed;
-		ControlRGB color1, color2, color3;
+		ControlRange<uint8_t, 255> speed;
+		ControlRGB colors[3];
 		ControlPushButton next, prev;
-
 	};
 
-#define SEGMENT_ITEM(id, name, start, stop, reverse) Segment SEG(name);
+	Segment segments[SEGMENTS_COUNT];
+	void segmentsSyncWith(uint8_t id);
+
+#define SEGMENT_ITEM(id, name, start, stop, reverse) Segment& SEG(name) = segments[id];
 	SEGMENTS
 #undef SEGMENT_ITEM
 
--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,34 @@ LED_METHOD strip(LED_COUNT);
 NeoAnimationFX<LED_METHOD> leds(strip);
 static Artnet artnet;
 
-void led_toggle() {
-#ifdef BOARD_LED
-	digitalWrite(BOARD_LED, !digitalRead(BOARD_LED));
+void board_led_blink(float period) {
+	blink_timer.attach(period, []() {
+			#ifdef BOARD_LED
+			digitalWrite(BOARD_LED, !digitalRead(BOARD_LED));
+			#endif
+		});
+}
+
+void board_button_handle() {
+#ifdef BOARD_BUTTON
+	static uint32_t pressed = 0;
+	static uint32_t last_time = 0;
+	auto now = millis();
+	if (now - last_time < 25)
+		return;
+	bool state = !digitalRead(BOARD_BUTTON);
+	if (state) {
+		if (!pressed)
+			log("pressed");
+		pressed++;
+	}
+	else {
+		if (pressed > 40)
+			log("long");
+		pressed = 0;
+	}
+	last_time = now;
 #endif
-}
-
-void led_blink(float period) {
-	blink_timer.attach(period, led_toggle);
-}
-
-void led_init() {
-	log("initializing strip with %d LEDs, color order %s", LED_COUNT, stringify(LED_COLOR_ORDER));
-
-	leds.init();
-	controls.begin();
-	leds.start();
 }
 
 uint16_t convertSpeed(uint8_t mcl_speed) {
@@ -45,10 +57,34 @@ static inline RgbColor convertColor(const Core::RgbColor& color) {
 	return RgbColor { color.R, color.G, color.B };
 }
 
-void led_setcolor(uint8_t seg, uint8_t n, const Core::RgbColor& val) {
-	auto c = convertColor(val);
-	log("seg %d color %d = %x", seg, n, HtmlColor(c).Color);
-	leds.setColor(seg, n, c);
+void leds_init() {
+	log("initializing strip with %d LEDs, color order %s", LED_COUNT, stringify(LED_COLOR_ORDER));
+
+	leds.init();
+	leds.setNumSegments(SEGMENTS_COUNT);
+	#define SEGMENT_ITEM(id, name, start, stop, reverse) \
+		leds.setSegment(id,  start, stop, \
+				controls.segments[id].mode, \
+				convertColor(controls.segments[id].colors[0]), \
+				convertSpeed(controls.segments[id].speed), reverse); \
+		leds.setColor(id, 1, convertColor(controls.segments[id].colors[1])); \
+		leds.setColor(id, 2, convertColor(controls.segments[id].colors[2]));
+		SEGMENTS
+	#undef SEGMENT_ITEM
+	leds.start();
+}
+
+void segments_sync(uint8_t seg, std::function<void(Controls::Segment&)> func) {
+	static bool sync_in_progress;
+	if (sync_in_progress || !controls.sync)
+		return;
+	sync_in_progress = true;
+	for (int i = 0; i < SEGMENTS_COUNT; i++) {
+		if (controls.segments[i].id == seg)
+			continue;
+		func(controls.segments[i]);
+	}
+	sync_in_progress = false;
 }
 
 /* Artnet */
@@ -86,57 +122,85 @@ void artnet_init()
 /* Controls */
 Controls::Controls() :
 	ControlsBase(),
-	artnet("artnet", "ArtNet enabled", 0, false,
+#ifdef BOARD_RELAY
+	relay("relay", "Relay", 0, false,
+		[](bool val) {
+			log("relay = %d", val);
+			digitalWrite(BOARD_RELAY, val);
+		}),
+#endif
+	artnet("artnet", "ArtNet enabled", 5, false,
 		[](bool val) {
 			log("artnet = %d", val);
 		}),
-	brightness("brightness", "Brightness", 1, 255,
+	brightness("brightness", "Brightness", 6, 255,
 		[](uint8_t val) {
 			leds.setBrightness(val);
 		}),
-	trigger("trigger", "Trigger", 2,
+	trigger("trigger", "Trigger", 7,
 		[](bool val) {
 			leds.trigger();
 		}),
-#define SEGMENT_ITEM(id, name, ...) SEG(name)(stringify(name), id),
+	sync("sync", "Sync", 8, true,
+		[](bool val) {
+			log("sync = %d", val);
+			segments_sync(0, [](Segment& seg) {
+				const auto& src = controls.segments[0];
+				seg.mode = src.mode;
+				seg.speed = src.speed;
+				for (uint8_t i = 0; i < NUM_COLORS; i++) {
+					seg.colors[i] = src.colors[i];
+				}
+			});
+		}),
+	segments {
+#define SEGMENT_ITEM(id, name, ...) Segment(stringify(name), id),
 	SEGMENTS
 #undef SEGMENT_ITEM
+	},
 	_dummy()
 {}
 
-void Controls::begin() {
-	leds.setNumSegments(2);
-	#define SEGMENT_ITEM(id, name, start, stop, reverse) \
-		leds.setSegment(id,  start, stop, SEG(name).mode, convertColor(SEG(name).color1), SEG(name).speed, reverse); \
-		leds.setColor(id, 1, convertColor(SEG(name).color2)); \
-		leds.setColor(id, 2, convertColor(SEG(name).color3));
-		SEGMENTS
-	#undef SEGMENT
+static void setcolor_helper(uint8_t seg, uint8_t n, const Core::RgbColor& val) {
+	auto c = convertColor(val);
+	log("seg %d color %d = %x", seg, n, HtmlColor(c).Color);
+	leds.setColor(seg, n, c);
+	segments_sync(seg, [n, val](Controls::Segment& seg) {
+		seg.colors[n] = val;
+	});
 }
 
 Controls::Segment::Segment(const std::string&& prefix, uint8_t _id) :
 	id(_id),
 	mode(prefix + "_mode", "Animation mode", (id+1)*10+1,
-			FX_MODE_RAINBOW,
+			FX_MODE_BREATH,
 			[this](uint8_t val) {
 				log("seg %d mode = %d", id, val);
 				leds.setMode(id, val);
+				segments_sync(id, [val](Segment& seg) {
+					seg.mode = val;
+				});
 			}),
 	speed(prefix + "_speed", "Animation speed", (id+1)*10+2,
-			DEFAULT_SPEED,
+			128,
 			[this](uint16_t val) {
 				log("seg %d speed = %d", id, val);
 				leds.setSpeed(id, convertSpeed(val));
+				segments_sync(id, [val](Segment& seg) {
+					seg.speed = val;
+				});
 			}),
-	color1(prefix + "_color1", "Color 1",  (id+1)*10+3,
-			Core::RgbColor{255, 0, 0},
-			std::bind(led_setcolor, id, 0, _1)),
-	color2(prefix + "_color2", "Color 2", (id+1)*10+4,
-			Core::RgbColor{0, 255, 0},
-			std::bind(led_setcolor, id, 1, _1)),
-	color3(prefix + "_color3", "Color 3", (id+1)*10+5,
-			Core::RgbColor{0, 0, 255},
-			std::bind(led_setcolor, id, 2, _1)),
+	colors {
+		ControlRGB(prefix + "_color1", "Color 1",  (id+1)*10+3,
+				Core::RgbColor{255, 0, 0},
+				std::bind(setcolor_helper, id, 0, _1)),
+		ControlRGB(prefix + "_color2", "Color 2", (id+1)*10+4,
+				Core::RgbColor{0, 255, 0},
+				std::bind(setcolor_helper, id, 1, _1)),
+		ControlRGB(prefix + "_color3", "Color 3", (id+1)*10+5,
+				Core::RgbColor{0, 0, 255},
+				std::bind(setcolor_helper, id, 2, _1))
+	},
 	next(prefix + "_next", "Next mode", (id+1)*10+6,
 			[this](bool val) {
 				log("seg %d next", id);
@@ -163,26 +227,28 @@ void setup()
 #ifdef BOARD_BUTTON
 	pinMode(BOARD_BUTTON, INPUT_PULLUP);
 #endif
+#ifdef BOARD_RELAY
+	pinMode(BOARD_RELAY, OUTPUT);
+#endif
 
-	led_init();
+	leds_init();
 
-	 Core::onConfigMode = []( {
-		led_blink(0.2);
+	Core::onConfigMode = []() {
+		board_led_blink(0.2);
 		uint16_t i;
 		for (i = 0; i < leds.numPixels(); i++) {
 			leds.setPixelColor(i, 0, 0, 255);
 		}
 		leds.show();
 	};
-
+	
 	Core::onConnect = []() {
-		led_blink(1);
+		board_led_blink(1);
 	};
-
+	
 	Core::onDisconnect = []() {
-		led_blink(0.1);
+		board_led_blink(0.1);
 	};
-
 
 	artnet_init();
 
@@ -198,4 +264,5 @@ void loop() {
 	else {
 		leds.service();
 	}
+	board_button_handle();
 }
